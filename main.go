c72de9f9bf0f6045be16c1e71a0f025efc94c55b
Marshall_USD <alimjan010206@gmail.com>
2025-11-04 14:23:23 +0500

    Add files via upload
    first code in Golang

---
+++ main.go
@@ -0,0 +1,16 @@
+ package main
+
+ import "fmt"
+
+ func main(){
+	
+
+	var x int = 5
+	var y int = 3
+	var result int = x*y
+	fmt.Println(result)
+	var strix string = "My name is GOLANG"
+	fmt.Println(strix)
+ }
+
+ 
