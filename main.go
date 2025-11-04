67ecf5210f614eae0675ab12f5b88386e594554a
Marshall_USD <alimjan010206@gmail.com>
2025-11-04 14:28:56 +0500

    Add files via upload
    added new := operators

--- main.go
+++ main.go
@@ -5,12 +5,17 @@
  func main(){
 	
 
-	var x int = 5
+/*	var x int = 5
 	var y int = 3
 	var result int = x*y
 	fmt.Println(result)
 	var strix string = "My name is GOLANG"
-	fmt.Println(strix)
+	fmt.Println(strix)*/
+
+	x :=5
+	y :=3
+	result:=x*y
+	fmt.Println(result)
  }
 
  
