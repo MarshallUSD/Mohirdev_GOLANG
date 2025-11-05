174e78940771166389f99a596309d02799a4e3d7
Marshall_USD <alimjan010206@gmail.com>
2025-11-05 16:03:39 +0500

    Add files via upload

---
+++ algo.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+	var num1 float32= 32.25
+	var num2 float32= 45.12
+	var result float32 =num1+num2
+	fmt.Println("The result od addition is:", result)
+
+}
+
